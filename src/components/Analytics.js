d7f9baef41636c43ec2d50590a2c61dc64d8c708
juan carlos macias <juancmaciassalvador@gmail.com>
2024-09-02 16:22:18 +0200

    Algunas mejoras de configuración

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -3,6 +3,11 @@ import ReactGA from "react-ga4";
 const Analytics = (onAction) => {
     console.log("enviar "+ onAction)
     ReactGA.initialize("G-98DNV82Z6L");
-    ReactGA.send({ hitType: "Web", page: "CV - JCMS", title: onAction });
+    ReactGA.send({ hitType: "pageview", page: "/my-path", title: onAction });
+    ReactGA.event({
+        category: 'User',
+        action: onAction,
+        label: onAction,
+    });
 }
 export default Analytics;
