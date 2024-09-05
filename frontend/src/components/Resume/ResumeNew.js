f6713fd25fd8788c2ffc17578059fc93ac6c2f93
juan carlos macias <juancmaciassalvador@gmail.com>
2024-09-05 17:19:21 +0200

    Cambios para .pdf

--- frontend/src/components/Resume/ResumeNew.js
+++ frontend/src/components/Resume/ResumeNew.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/pdf/cv.pdf";
+//import pdf from "../../Assets/cv.pdf";
 import Analytics from "../Analytics";
 import LoadingSpinner from "../Loading/LoadingSpinner";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
+//import { Document, Page, pdfjs } from "react-pdf";
+//import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+//pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+//pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+//pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
+/*
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+*/
 function ResumeNew() {
   Analytics("CV")
   const [width, setWidth] = useState(1200);
   const [ isLoading, setIsLoading] = useState(true)
-
+  const pdf = "../../Assets/pdf/cv.pdf"
   useEffect(() => {
     setWidth(window.innerWidth);
     setIsLoading(false)
@@ -31,11 +38,13 @@ function ResumeNew() {
             &nbsp;Descargar CV
           </Button>
         </Row>
-
         <Row className="resume">
+            <embed src={pdf} scale={width > 786 ? 1.7 : 0.6} className="pdfcss"  type="application/pdf" />
+          {/*
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+          */}
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
